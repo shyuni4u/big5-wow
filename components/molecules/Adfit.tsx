2415ce08a4ef0a17fe588284bfbed11aa85e786f
shyuni4u <shyuniz@naver.com>
2021-06-01 14:19:23 +0900

    update Adfit

--- components/molecules/Adfit.tsx
+++ components/molecules/Adfit.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 export const Adfit: React.FC = () => {
   return (
     <>
-      <ins className="kakao_ad_area" data-ad-unit="DAN-MeIbICppOyhqMX2E" data-ad-width="320" data-ad-height="50"></ins>
+      <ins className="kakao_ad_area" style={{ display: 'none' }} data-ad-unit="DAN-MeIbICppOyhqMX2E" data-ad-width="320" data-ad-height="50"></ins>
       <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
     </>
   );
