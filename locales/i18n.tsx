f6ec3caafb1c4729bc15ccccde70abafe17ee00a
shyuni4u <shyuniz@naver.com>
2021-06-08 22:08:08 +0900

    fix text.

--- locales/i18n.tsx
+++ locales/i18n.tsx
@@ -154,7 +154,7 @@ const resources = {
         retry: '다시하기',
         you: '당신의 성향',
         likeyoucount: '성향별 직업 수',
-        likeyouratio: '직업별 성향 비율 (평균: 2.78%)',
+        likeyouratio: '직업별 성향 비율 (성향별 직업 수 / 직업총합)',
         totalcount: '직업별 총합',
         testcount: '같은 결과를 가진 데이터 수',
         thankyou: '성향 테스트에 응해주셔서 감사합니다.',
