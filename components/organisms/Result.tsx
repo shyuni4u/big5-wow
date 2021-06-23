41dee05dac8a64e184e6bb54908f67003a9da0df
shyuni4u <shyuniz@naver.com>
2021-06-23 23:30:59 +0900

    fix option.

--- components/organisms/Result.tsx
+++ components/organisms/Result.tsx
@@ -308,7 +308,7 @@ export const Result: React.FC = () => {
       // Step 6: train your neural network
       const trainingOptions = {
         epochs: 10,
-        batchSize: 50
+        batchSize: 10
       };
       // Step 7: use the trained model
       const finishedTraining = () => {
